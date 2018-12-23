c5423c5e5d5b9c98cf7ccf909406881801d22afa
louiemontes <louiemontes@gmail.com>
2018-12-23 08:34:36 -0700

    get upto navbar complete

---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const SignedInLinks = () => {
+  return (
+    <ul className="right">
+      <li>
+        <NavLink to="/">New Target</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Log out</NavLink>
+      </li>
+      <li>
+        <NavLink to="/" className="btn btn-floating yellow darken-2">
+          LM
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default SignedInLinks;
