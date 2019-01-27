d0d9a81d6ea63249535383afd02b8cd1923988e4
louiemontes <louiemontes@gmail.com>
2019-01-27 00:34:45 -0700

    finish create target

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import TargetDetails from "./components/targets/TargetDetails";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
+import CreateTarget from "./components/targets/CreateTarget";
 
 class App extends Component {
   render() {
@@ -17,6 +18,7 @@ class App extends Component {
             <Route path="/target/:id" component={TargetDetails} />
             <Route path="/signin" component={SignIn} />
             <Route path="/signup" component={SignUp} />
+            <Route path="/create" component={CreateTarget} />
           </Switch>
         </div>
       </BrowserRouter>
