cf7b5a846d3b5a2815c92e5ab181aa6c4e550550
louiemontes <louiemontes@gmail.com>
2019-01-27 00:22:35 -0700

    finish signin mockup

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,20 +1,37 @@
 import React, { Component } from "react";
 
 class SignIn extends Component {
-  state = {};
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
 
   render() {
     return (
       <div className="container">
-        <form onSubmit className="white">
+        <form onSubmit={this.handleSubmit} className="white">
           <h5 className="gray-text text-darken-3">Sign In</h5>
           <div className="input-field">
             <label htmlFor="email">Email</label>
-            <label type="email" id="email" onChange />
+            <input type="email" id="email" onChange={this.handleChange} />
           </div>
           <div className="input-field">
             <label htmlFor="password">Password</label>
-            <label htmlFor="" />
+            <input type="password" id="password" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <button className="btn yellow darken-2 z-depth-0">Login</button>
           </div>
         </form>
       </div>
